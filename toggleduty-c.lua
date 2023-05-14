9e842bf89067f095389068403c10a73247aa9d8b
Dozy <coreyj0605@gmail.com>
2023-05-14 01:42:24 +0100

    My updatees

--- toggleduty-c.lua
+++ toggleduty-c.lua
@@ -7,96 +7,52 @@ RegisterCommand("duty", function()
         TriggerServerEvent("police:server:UpdateBlips")
         TriggerServerEvent("QBCore:ToggleDuty")
         SetPedArmour(PlayerPedId(), 100)
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "ambulance" then
+    elseif playerData.job.name == "ambulance" then
         TriggerServerEvent("police:server:UpdateBlips")
         TriggerServerEvent("QBCore:ToggleDuty")
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "mechanic" then
+    elseif playerData.job.name == "mechanic" then
         TriggerServerEvent("QBCore:ToggleDuty")
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "real estate" then
+    elseif playerData.job.name == "real estate" then
         TriggerServerEvent("QBCore:ToggleDuty")
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "taxi" then
+    elseif playerData.job.name == "taxi" then
         TriggerServerEvent("QBCore:ToggleDuty")
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "bus" then
+    elseif playerData.job.name == "bus" then
         TriggerServerEvent("QBCore:ToggleDuty")
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "cardealer" then
+    elseif playerData.job.name == "cardealer" then
         TriggerServerEvent("QBCore:ToggleDuty")
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "judge" then
+    elseif playerData.job.name == "judge" then
         TriggerServerEvent("QBCore:ToggleDuty")
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "lawyer" then
+    elseif playerData.job.name == "lawyer" then
         TriggerServerEvent("QBCore:ToggleDuty")
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "weazelnews" then
+    elseif playerData.job.name == "weazelnews" then
         TriggerServerEvent("QBCore:ToggleDuty")
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "trucker" then
+    elseif playerData.job.name == "trucker" then
         TriggerServerEvent("QBCore:ToggleDuty")
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "tow" then
+    elseif playerData.job.name == "tow" then
         TriggerServerEvent("QBCore:ToggleDuty")
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "garbage" then
+    elseif playerData.job.name == "garbage" then
         TriggerServerEvent("QBCore:ToggleDuty")
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "vineyard" then
+    elseif playerData.job.name == "vineyard" then
         TriggerServerEvent("QBCore:ToggleDuty")
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "hotdog" then
+    elseif playerData.job.name == "hotdog" then
         TriggerServerEvent("QBCore:ToggleDuty")
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "gallery" then
+    elseif playerData.job.name == "gallery" then
         TriggerServerEvent("QBCore:ToggleDuty")
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "burgershot" then
+    elseif playerData.job.name == "burgershot" then
         TriggerServerEvent("QBCore:ToggleDuty")
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "catcafe" then
+    elseif playerData.job.name == "catcafe" then
         TriggerServerEvent("QBCore:ToggleDuty")
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "pizza" then
+    elseif playerData.job.name == "pizza" then
         TriggerServerEvent("QBCore:ToggleDuty")
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "beanmachine" then
+    elseif playerData.job.name == "beanmachine" then
         TriggerServerEvent("QBCore:ToggleDuty")
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "vu" then
+    elseif playerData.job.name == "vu" then
         TriggerServerEvent("QBCore:ToggleDuty")
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "henhouse" then
+    elseif playerData.job.name == "henhouse" then
         TriggerServerEvent("QBCore:ToggleDuty")
-    end
-    local playerData = QBCore.Functions.GetPlayerData()
-    if playerData.job.name == "tunershop" then
+    elseif playerData.job.name == "tunershop" then
         TriggerServerEvent("QBCore:ToggleDuty")
+    elseif playerData.job.name == nil then 
+        print('You dont have a job lazy bum!')
     end
 end)
-
-
